64d6e15b1ead5a86829f76921932e41c577a8d4e
Anand Simmy <anandsimmy7@gmail.com>
2021-01-20 08:10:17 +0530

    added other validations

--- form-validator/script.js
+++ form-validator/script.js
@@ -16,18 +16,13 @@ const showError= (input, message) => {
     small.innerText= message
 }
 
-const isValidEmail= (email) => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
 const getFieldName= (input) => {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1)
 }
 
 const checkRequired= (inputArr) => {
     inputArr.forEach((input) => {
-        if(input.value === ''){
+        if(input.value.trim()  === ''){
             showError(input, `${getFieldName(input)} is required`)
         }else{
             showSuccess(input)
@@ -35,9 +30,38 @@ const checkRequired= (inputArr) => {
     })
 }
 
+const checkLength= (input, min, max) => {
+    if(input.value.length < min){
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+    }else if(input.value.length > max){
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`)
+    }else{
+        showSuccess(input)
+    }
+}
+
+const checkEmail= (input) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(input.value.trim())){
+        showSuccess(input)
+    }else{
+        showError(input, 'Email is not valid')
+    }
+}
+
+const checkPasswords= (input1, input2) => {
+    if(input1.value !== input2.value){
+        showError(input2, 'Passwords do not match')
+    }
+}
+
 //event listeners
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
     checkRequired([username, email, password, password2])
+    checkLength(username, 3, 15)
+    checkLength(password, 6, 25)
+    checkEmail(email)
+    checkPasswords(password, password2)
 })
