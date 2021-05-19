3003f82a2bb1893c4b9ef003575d436a2f6e5cf4
Anand Simmy <anandsimmy7@gmail.com>
2021-05-19 10:00:14 +0530

    added random letter list

--- hangman/script.js
+++ hangman/script.js
@@ -4,3 +4,17 @@ const letters= ['programmer', 'computer', 'keyboard', 'internet', 'facebook']
 
 const randomLetter= letters[Math.floor(Math.random() * letters.length)]
 
+const setLettersOfRandomWord= () => {
+    for(let i=0; i < randomLetter.length; i++){
+        const letterElement= document.createElement('div')
+        letterElement.classList.add('letter')
+        letterElement.textContent= randomLetter[i]
+        wordContainer.appendChild(letterElement)
+    }
+}
+
+setLettersOfRandomWord()
+
+document.addEventListener('keypress', (e) => {
+    console.log(e.key)
+})
