139d546819648dfdf70c2f0faed5856c4be5946a
Anand Simmy <anandsimmy7@gmail.com>
2021-05-19 09:59:46 +0530

    script added

--- hangman/script.js
+++ hangman/script.js
@@ -1,1 +1,6 @@
 const wordContainer= document.getElementById('word')
+
+const letters= ['programmer', 'computer', 'keyboard', 'internet', 'facebook']
+
+const randomLetter= letters[Math.floor(Math.random() * letters.length)]
+
