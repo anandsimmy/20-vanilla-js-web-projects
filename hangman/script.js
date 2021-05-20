5aea32be6b127a0109128b4ff61bab455b96feec
Anand Simmy <anandsimmy7@gmail.com>
2021-05-20 09:07:40 +0530

    added letter adding part

--- hangman/script.js
+++ hangman/script.js
@@ -1,20 +1,37 @@
 const wordContainer= document.getElementById('word')
 
-const letters= ['programmer', 'computer', 'keyboard', 'internet', 'facebook']
+const letters= [
+    'programmer',
+    'computer',
+    'keyboard',
+    'internet',
+    'facebook',
+    'aquarium',
+    'flowers',
+    'linkedin'
+]
 
 const randomLetter= letters[Math.floor(Math.random() * letters.length)]
 
-const setLettersOfRandomWord= () => {
+const setHiddenRandomWord= () => {
     for(let i=0; i < randomLetter.length; i++){
         const letterElement= document.createElement('div')
-        letterElement.classList.add('letter')
-        letterElement.textContent= randomLetter[i]
+        letterElement.classList.add('letter')       
         wordContainer.appendChild(letterElement)
     }
 }
 
-setLettersOfRandomWord()
+const setLettersOfRandomWord= (key) => {
+    for(let i=0; i < randomLetter.length; i++){
+        if(randomLetter[i]===key){
+            wordContainer.children[i].textContent= key
+        }
+    }
+}
+
+setHiddenRandomWord()
 
 document.addEventListener('keypress', (e) => {
     console.log(e.key)
+    setLettersOfRandomWord(e.key)
 })
