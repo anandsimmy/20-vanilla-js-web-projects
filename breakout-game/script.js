52621ffa37ccb0acab820596b73dc4c7f392759d
Anand Simmy <anandsimmy7@gmail.com>
2021-06-27 17:55:22 +0530

    #minor change

--- breakout-game/script.js
+++ breakout-game/script.js
@@ -1,6 +1,6 @@
 const rulesDiv= document.getElementById('rules')
 const showRulesButton= document.getElementById('rules-btn')
-// const closeButton= document.getElementById('close-btn')
+const closeButton= document.getElementById('close-btn')
 
 showRulesButton.addEventListener('click', () => {
     rulesDiv.classList.add('show')
